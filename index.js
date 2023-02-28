265ac1053c10a4fe968873937d17f94d0efd5672
mitralola716 <126588109+mitralola716@users.noreply.github.com>
2023-03-01 05:48:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const sessionName = "yusril";
 const donet = "https://saweria.co/sansekai";
-const owner = ["6287878817169"]; // Put your number here ex: ["62xxxxxxxxx"]
+const owner = ["6281326482638"]; // Put your number here ex: ["62xxxxxxxxx"]
 const {
   default: sansekaiConnect,
   useMultiFileAuthState,
